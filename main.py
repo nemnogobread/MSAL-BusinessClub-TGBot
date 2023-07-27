90c2705c394dd92adbf5d3b1ddb709997e3cd92f
Gleb <glebmiloslavov@gmail.com>
2023-07-28 00:32:10 +0300

    added skip pending & firstname greeting

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import telebot
 
-bot = telebot.TeleBot('6556691353:AAET9cz_wPIog5m2n25D8nnQXy-h9GXCIlk')
+bot = telebot.TeleBot('6556691353:AAET9cz_wPIog5m2n25D8nnQXy-h9GXCIlk', skip_pending=True)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Это тестовый бот для бизнес-клуба МГЮА')
 
 
 bot.polling(none_stop=True)
