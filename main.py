51b1e45fa3873118396a395d57b2ce79e98cfee2
Gleb <125480807+nemnogobread@users.noreply.github.com>
2023-07-29 21:36:23 +0300

    added splite3, users table, rebuilded buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import telebot
 from telebot import types
 
@@ -6,22 +7,49 @@ bot = telebot.TeleBot('6556691353:AAET9cz_wPIog5m2n25D8nnQXy-h9GXCIlk', skip_pen
 
 @bot.message_handler(commands=['start', 'hello'])
 def start_message(message):
+
+    conn = sqlite3.connect('database.sql')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+                id int auto_increment primary key,
+                name varchar(50),
+                surname varchar(50),
+                institute varchar(50),
+                age INTEGER
+                )""")
+    conn.commit()
+    cur.close()
+    conn.close()
+
     markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('/hello')
-    markup.add(btn1)
-    btn2 = types.KeyboardButton('/help')
-    markup.add(btn2)
-    btn3 = types.KeyboardButton('/admin_message_info')
-    markup.add(btn3)
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Это тестовый бот для бизнес-клуба МГЮА', reply_markup=markup)
+    btn1 = types.KeyboardButton('👋 Поздороваться')
+    btn2 = types.KeyboardButton('📲 Зарегестрироваться')
+    btn3 = types.KeyboardButton('👀 Просмотреть пользователей')
+    markup.row(btn1, btn2)
+    markup.row(btn3)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Я тестовый бот для бизнес-клуба МГЮА', reply_markup=markup)
  
 
-@bot.message_handler(commands=['admin_message_info'])
-def send_admin_info(message):
-    bot.send_message(message.chat.id, message)
-
-@bot.message_handler(commands=['help'])
-def send_admin_info(message):
-    bot.send_message(message.chat.id, 'Упс, пока что здесь ничего нет...')
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == '👋 Поздороваться'):
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Я тестовый бот для бизнес-клуба МГЮА')
+    
+    elif(message.text == '📲 Зарегестрироваться'):
+        bot.send_message(message.chat.id, 'Ой-ой')
+    
+    elif message.text == "👀 Просмотреть пользователей":
+        bot.send_message(message.chat.id, 'Упс, меня пока на это не запрограммировали')
+    
+    elif (message.text == "Вернуться в главное меню"):
+        markup = types.ReplyKeyboardMarkup()
+        btn1 = types.KeyboardButton('👋 Поздороваться')
+        btn2 = types.KeyboardButton('📲 Зарегестрироваться')
+        btn3 = types.KeyboardButton('👀 Просмотреть пользователей')
+        markup.row(btn1, btn2)
+        markup.row(btn3)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text="На такую комманду я не запрограммирован..")
 
 bot.polling(none_stop=True)
